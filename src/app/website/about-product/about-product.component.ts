729671bad0805e6c0d3e11f19a47a84f041b0299
shahzodbekxoltorayev <sh.xoltorayev@gamil.com>
2020-02-20 15:27:29 +0500

    about product

--- src/app/website/about-product/about-product.component.ts
+++ src/app/website/about-product/about-product.component.ts
@@ -51,6 +51,53 @@ export class AboutProductComponent implements OnInit {
   );
     });
 
+
+    function init() {
+      document.getElementById("clickMe1").addEventListener("click", function() {
+        document.getElementById("bikini-display1").style.display = "block";
+        document.getElementById("bikini-display3").style.display = "none";
+        document.getElementById("bikini-display4").style.display = "none";
+        document.getElementById("bikini-display2").style.display = "none";
+      });
+    
+      document.getElementById("clickMe2").addEventListener("click", function() {
+        document.getElementById("bikini-display2").style.display ="block";
+        document.getElementById("bikini-display1").style.display ="none";
+        document.getElementById("bikini-display3").style.display ="none";
+        document.getElementById("bikini-display4").style.display ="none";
+      
+      });
+    
+      document.getElementById("clickMe3").addEventListener("click", function() {
+        document.getElementById("bikini-display3").style.display ="block";
+        document.getElementById("bikini-display1").style.display ="none";
+        document.getElementById("bikini-display4").style.display ="none";
+        document.getElementById("bikini-display2").style.display ="none";
+      });
+    
+      document.getElementById("clickMe4").addEventListener("click", function() {
+        document.getElementById("bikini-display4").style.display ="block";
+        document.getElementById("bikini-display1").style.display ="none";
+        document.getElementById("bikini-display3").style.display ="none";
+        document.getElementById("bikini-display2").style.display ="none";
+      });
+    }
+    init();
+    
+    $(".bikini-thumbnail img").click(function() {
+      //Remove the my-list--selected class from any elements that already have it
+      $('.thumbnail-clicked').removeClass('thumbnail-clicked');
+      //Add the .border-bottom class back to any element that is missing it
+      $('.thumbnail-clicked').addClass('bikini-thumbnails');
+      //Add the my-list--selected class to the clicked element
+      $(this).addClass('thumbnail-clicked');
+      //Remove the border-bottom class from the clicked element
+      $(this).find('.bikini-thumbnails').removeClass('bikini-thumbnails');
+      });
+
+
+      
+
   }
 
 }
